a816df7f3d26b63cfb31b1ba48635ba50ecf3484
Sam <s.olubode@alustudent.com>
2024-06-06 15:02:22 +0100

    resolve

--- net_salary_calculator.c
+++ net_salary_calculator.c
@@ -2,8 +2,8 @@
 
 double deduct_tax_on_salary(double*);
 double *deduct_medication_insurance(double*);
-// double deduct_maternity_leave(int*);
-// double deduct_social_security(int*);
+double *deduct_maternity_leave(double*);
+double *deduct_social_security(double*);
 
 double gross_salary = 100000;
 double * pgs = &gross_salary;
@@ -14,7 +14,10 @@ void main()
 	double * pns = &net_salary;
 
 	*pns = deduct_tax_on_salary(pgs);
+	printf("After Tax: %f\n", *pns); 
 	pns = deduct_medication_insurance(pns);
+	pns = deduct_maternity_leave(pns);
+	pns = deduct_social_security(pns);
 
 	printf("Net Salary is: %.2f\n", *pns);
 }
@@ -40,10 +43,27 @@ double deduct_tax_on_salary(double *pgs)
 
 double *deduct_medication_insurance(double *pns)
 {
-	double insurance = 0.05;
+	double insurance = (5 / 100.0);
 
 	*pns = *pns * (1 - insurance);
+	printf("After Insurance: %f\n", *pns); 
 	return pns;
 }
-// double deduct_maternity_leave(int*);
-// double deduct_social_security(int*);
+
+double *deduct_maternity_leave(double *pns)
+{
+	double maternity_leave = (0.03 / 100.0);
+
+	*pns = *pns * (1 - maternity_leave);
+	printf("After Maternity: %f\n", *pns); 
+	return pns;
+}
+
+double *deduct_social_security(double *pns)
+{
+	double social_security = (3 / 100.0);
+
+	*pns = *pns * (1 - social_security);
+	printf("After Social: %f\n", *pns); 
+	return pns;
+}
